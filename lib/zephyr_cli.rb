9ec1eea29febc516808ef9d6fdcce3fb0cb90ca2
Chris Davis <chrisdavis@Chriss-MacBook-Air-2.local>
2024-08-02 17:49:56 -0400

    added base command and subcommmand structure for cli

--- lib/zephyr_cli.rb
+++ lib/zephyr_cli.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
-require 'zephyr_ruby'
-require 'thor'
-require 'nokogiri'
+require "zephyr_ruby"
+require "thor"
+require "nokogiri"
 require_relative "zephyr_cli/version"
 require_relative "zephyr_cli/test_cycles"
 require_relative "zephyr_cli/test_plans"
-
-@zephyr_client = ZephyrRuby::Client.new(ENV["ZEPHYR_API_KEY"])
+require_relative "zephyr_cli/helpers/cli_helpers"
 
 # CLI for creating test cycles and test plans in zephyr
 class CLI < Thor
+  @zephyr_client = ZephyrRuby::Client.new(ENV["ZEPHYR_API_KEY"])
+
   desc "test_cycles", "Test cycles commands"
   subcommand "test_cycles", TestCycles
 
--- lib/zephyr_cli/test_cycles.rb
+++ lib/zephyr_cli/test_cycles.rb
@@ -5,23 +5,25 @@ class TestCycles < Thor
   desc "upload_junit_results", "Upload test results in junit format"
   method_option :project_id, type: :numeric, required: true
   method_option :file, type: :string, required: true
-  method_option :auto_close_cycle, type: :boolean, required: false
-  method_option :auto_create_test_cases, type: :boolean, required: false
   method_option :name, type: :string, required: true
   method_option :description, type: :string, required: false
-  def upload_junit_results(test_cycle_id, junit_xml_file, close_cycle)
-    # TODO: Implement logic for parsing junit xml file and uploading test results to Zephyr
+  method_option :auto_create_test_cases, type: :boolean, required: false
+  def upload_junit_results(project_id, file, auto_close_cycle, name, description, auto_create_test_cases)
+    payload = generate_automations_payload(project_id, file, auto_close_cycle, name, description,
+                                           auto_create_test_cases)
+    @zephyr_client.create_automation_junit(payload)
   end
 
   desc "upload_cucumber_results", "Upload test results in cucumber format"
   method_option :project_id, type: :numeric, required: true
   method_option :file, type: :string, required: true
-  method_option :auto_close_cycle, type: :boolean, required: false
-  method_option :auto_create_test_cases, type: :boolean, required: false
   method_option :name, type: :string, required: true
   method_option :description, type: :string, required: false
-  def upload_cucumber_results(test_cycle_id, cucumber_format)
-    # TODO: Implement logic for parsing test results and uploading test results to in cucumber format
+  method_option :auto_create_test_cases, type: :boolean, required: false
+  def upload_cucumber_results(_test_cycle_id, _cucumber_format)
+    payload = generate_automations_payload(project_id, file, auto_close_cycle, name, description,
+                                           auto_create_test_cases)
+    @zephyr_client.create_automation_cucumber(payload)
   end
 
   desc "create", "Create test cycle"
@@ -31,7 +33,8 @@ class TestCycles < Thor
   method_option :start_date, type: :string, required: true
   method_option :end_date, type: :string, required: true
   def create(name, description, start_date, end_date, project_id)
-    # TODO: Implement logic for creating test cycle
+    payload = generate_test_cycle_payload(name, description, start_date, end_date, project_id)
+    @zephyr_client.create_test_cycle(payload)
   end
 
   desc "update", "Update test cycle"
@@ -41,9 +44,8 @@ class TestCycles < Thor
   method_option :description, type: :string, required: true
   method_option :start_date, type: :string, required: true
   method_option :end_date, type: :string, required: true
-  def update(test_cycle_id, name, description, start_date, end_date, project_id)
-    # TODO: Implement logic for updating test cycle
+  def update(_test_cycle_id, name, description, start_date, end_date, project_id)
+    payload = generate_test_cycle_payload(name, description, start_date, end_date, project_id)
+    @zephyr_client.update_test_cycle(payload)
   end
 end
-
-ZephyrCLI.start(ARGV)
