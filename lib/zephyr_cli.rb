5e83b037a5b106b3d2bbaf552f2906e08a9c0eb1
Chris <33081107+cdavis-personal@users.noreply.github.com>
2024-08-10 18:15:30 -0400

    added base command and subcommmand structure for cli (#5)
    Signed-off-by: Chris <33081107+cdavis-personal@users.noreply.github.com>
    Co-authored-by: Chris Davis <chrisdavis@Chriss-MacBook-Air-2.local>

--- lib/zephyr_cli.rb
+++ lib/zephyr_cli.rb
@@ -8,10 +8,10 @@ require_relative "zephyr_cli/test_cycles"
 require_relative "zephyr_cli/test_plans"
 require_relative "zephyr_cli/helpers/cli_helpers"
 
-@zephyr_client = ZephyrRuby::Client.new(ENV["ZEPHYR_API_KEY"])
-
 # CLI for creating test cycles and test plans in zephyr
 class CLI < Thor
+  @zephyr_client = ZephyrRuby::Client.new(ENV["ZEPHYR_API_KEY"])
+
   desc "test_cycles", "Test cycles commands"
   subcommand "test_cycles", TestCycles
 
